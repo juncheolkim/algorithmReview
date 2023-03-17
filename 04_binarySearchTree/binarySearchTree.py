0253f611ca595a23b71005d6b2192f929c4337da
김준철 <kimjoon889@gamil.com>
2023-03-17 15:09:56 +0900

    feat : implement contains method

--- 04_binarySearchTree/binarySearchTree.py
+++ 04_binarySearchTree/binarySearchTree.py
@@ -29,22 +29,31 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
 
+    def contains(self, value):
+        temp = self.root
+        while (temp is not None):
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+        
 
 
 
 my_tree = BinarySearchTree()
-my_tree.insert(2)
-my_tree.insert(1)
-my_tree.insert(3)
+my_tree.insert(47)
+my_tree.insert(21)
+my_tree.insert(76)
+my_tree.insert(18)
+my_tree.insert(27)
+my_tree.insert(52)
+my_tree.insert(82)
 
-"""
-    THE LINES ABOVE CREATE THIS TREE:
-                 2
-                / \
-               1   3
-"""
+print('BST Contains 27:')
+print(my_tree.contains(27))
 
-
-print('Root:', my_tree.root.value)            
-print('Root->Left:', my_tree.root.left.value)        
-print('Root->Right:', my_tree.root.right.value)        
+print('\nBST Contains 17:')
+print(my_tree.contains(17))
