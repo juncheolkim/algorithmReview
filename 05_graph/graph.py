26d3b35e85d58592d241b9b1610df057b4ff27b8
juncheolkim <kimjoon889@gmail.com>
2023-03-17 16:13:46 +0900

    feat : remove vertex

--- 05_graph/graph.py
+++ 05_graph/graph.py
@@ -29,21 +29,34 @@ class Graph:
             return True
         return False
 
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False        
+
+
 my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
 my_graph.add_vertex('C')
+my_graph.add_vertex('D')
 
 my_graph.add_edge('A','B')
-my_graph.add_edge('B','C')
-my_graph.add_edge('C','A')
+my_graph.add_edge('A','C')
+my_graph.add_edge('A','D')
+my_graph.add_edge('B','D')
+my_graph.add_edge('C','D')
 
-print('Graph before remove_edge():')
+
+print('Graph before remove_vertex():')
 my_graph.print_graph()
 
 
-my_graph.remove_edge('A','C')
+my_graph.remove_vertex('D')
 
 
-print('\nGraph after remove_edge():')
+print('\nGraph after remove_vertex():')
 my_graph.print_graph()
