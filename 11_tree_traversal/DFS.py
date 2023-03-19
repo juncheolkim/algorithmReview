fdd25aceea047c0db9b95361df60895e2bba1fc3
juncheolkim <kimjoon889@gmail.com>
2023-03-19 14:46:35 +0900

    feat : implement inorder

--- 11_tree_traversal/DFS.py
+++ 11_tree_traversal/DFS.py
@@ -82,6 +82,19 @@ class BinarySearchTree:
         traverse(self.root)
         return results
 
+    def dfs_in_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            results.append(current_node.value) 
+            if current_node.right is not None:
+                traverse(current_node.right)          
+        traverse(self.root)
+        return results
+        
+
+
 
 
 my_tree = BinarySearchTree()
@@ -93,4 +106,4 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.dfs_post_order())
+print(my_tree.dfs_in_order())
