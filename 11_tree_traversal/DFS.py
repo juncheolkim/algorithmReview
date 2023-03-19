6d8e769141a22e7368cfd274644576b9f39d8c3d
juncheolkim <kimjoon889@gmail.com>
2023-03-19 14:42:44 +0900

    feat : implement postorder

--- 11_tree_traversal/DFS.py
+++ 11_tree_traversal/DFS.py
@@ -71,6 +71,16 @@ class BinarySearchTree:
         return results
 
 
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
 
 
 
@@ -83,4 +93,4 @@ my_tree.insert(27)
 my_tree.insert(52)
 my_tree.insert(82)
 
-print(my_tree.dfs_pre_order())
+print(my_tree.dfs_post_order())
